d64c26023dcea54109bb2511037e0ff4ba7f51c0
caiobreno123 <caiobrenopiresduarte@gmail.com>
2026-02-23 17:36:25 +0000

    resolvendo conflitos do merge

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/index.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/index.js
@@ -1,14 +1,17 @@
 function validarPedido(pedido) {
+  if (!pedido.cliente) {
+    return false
+  }
 
-  if (!pedido) return false;
+  if (pedido.total <= 0) {
+    return false
+  }
 
-  if (!pedido.cliente) return false;
+  if (!pedido.itens || pedido.itens.length === 0) {
+    return false
+  }
 
-  if (pedido.total <= 0) return false;
-
-  if (!Array.isArray(pedido.itens) || pedido.itens.length === 0) return false;
-
-  return true;
+  return true
 }
 
 module.exports = validarPedido;
